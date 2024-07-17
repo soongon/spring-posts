ba62461e576d0f7d9fcb8f7629f04c24b4207717
KITRI <soongon@gmail.com>
2024-07-17 16:25:07 +0900

    로깅 시스템

--- src/main/java/kr/re/kitri/springposts/controller/PostController.java
+++ src/main/java/kr/re/kitri/springposts/controller/PostController.java
@@ -3,6 +3,8 @@ package kr.re.kitri.springposts.controller;
 import kr.re.kitri.springposts.model.Post;
 import kr.re.kitri.springposts.service.PostService;
 import kr.re.kitri.springposts.service.PostServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,6 +12,8 @@ import java.util.List;
 
 @RestController
 public class PostController {
+
+    private static final Logger log = LoggerFactory.getLogger(PostController.class);
 
     private final PostService postService;
 
@@ -19,6 +23,7 @@ public class PostController {
 
     @GetMapping("/posts")
     public List<Post> allPosts() {
+        log.debug("테스트 합니다. 로깅 좋아요..");
         return postService.viewAllPosts();
     }
 
--- src/main/java/kr/re/kitri/springposts/service/PostServiceImpl.java
+++ src/main/java/kr/re/kitri/springposts/service/PostServiceImpl.java
@@ -2,13 +2,19 @@ package kr.re.kitri.springposts.service;
 
 import kr.re.kitri.springposts.model.Post;
 import kr.re.kitri.springposts.repository.PostRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class PostServiceImpl implements PostService {
+
+    //private static final Logger log = LoggerFactory.getLogger(PostServiceImpl.class);
 
     private final PostRepository postRepository;
 
@@ -18,6 +24,7 @@ public class PostServiceImpl implements PostService {
 
     // 전체글 보기
     public List<Post> viewAllPosts() {
+        log.info("롬복 좋아요..");
         return postRepository.selectAllPost();
     }
     // 글 상세보기 by ID
