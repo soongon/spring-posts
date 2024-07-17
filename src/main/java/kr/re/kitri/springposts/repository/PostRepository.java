0a5bb1ee39f01459ceba5b2cd3ccaa4e001caf76
KITRI <soongon@gmail.com>
2024-07-17 17:11:14 +0900

    spring data 용 repository

--- src/main/java/kr/re/kitri/springposts/repository/PostRepository.java
+++ src/main/java/kr/re/kitri/springposts/repository/PostRepository.java
@@ -1,18 +1,27 @@
 package kr.re.kitri.springposts.repository;
 
 import kr.re.kitri.springposts.model.Post;
-import org.apache.ibatis.annotations.Mapper;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Optional;
 
 @Repository
-@Mapper
-public interface PostRepository {
+public interface PostRepository extends CrudRepository<Post, Long> {
 
-    List<Post> selectAllPost();
-    Post selectPostById(long postId);
-    void insertPost(Post post);
-    void updateLikesPlusOne(long postId);
+    // 전체보기
+    @Override
+    Iterable<Post> findAll();
+
+    // 상세보기
+    @Override
+    Optional<Post> findById(Long postId);
+
+    // 글 등록하기
+    @Override
+    Post save(Post post);
+
+    // 글 삭제
+    @Override
+    void deleteById(Long postId);
 }
